ae4b94c1af2b560e70396556300590a413fd0f11
Alex Mwangi <alex@stl-horizon.com>
2023-01-27 12:47:16 +0300

    React Fundamentals - React.createElement() Function

--- 00-react-tutorial-app/src/index.js
+++ 00-react-tutorial-app/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// JSX Syntax
+// function Greeting() {
+//   return <h2>My First Component</h2>;
+// }
+
+// createElement() function
 function Greeting() {
-  return <h2>My First Component</h2>;
+  return React.createElement('h2', {}, 'hello world');
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
