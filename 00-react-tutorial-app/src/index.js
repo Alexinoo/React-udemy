011739842ac6cd4c848ae7929c7a19e01c5542ba
Alex Mwangi <alex@stl-horizon.com>
2023-01-27 09:11:00 +0300

    React Fundamentals - My First Component

--- 00-react-tutorial-app/src/index.js
+++ 00-react-tutorial-app/src/index.js
@@ -1,5 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 function Greeting() {
   return <h2>My First Component</h2>;
 }
 
-export default Greeting;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// console.log(root);
+
+root.render(<Greeting />);
+
+/* same as root.render(<Greeting></Greeting>); */
