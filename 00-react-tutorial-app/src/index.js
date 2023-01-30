b014d3eaee27ebf9007d8621293c19084373a001
Alex Mwangi <alex@stl-horizon.com>
2023-01-30 16:04:58 +0300

    React Fundamentals - BookList Example

--- 00-react-tutorial-app/src/index.js
+++ 00-react-tutorial-app/src/index.js
@@ -1,27 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Greeting() {
+const BookList = () => {
   return (
-    <div>
-      <Person></Person>
-      <Message></Message>
-    </div>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
-}
+};
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
 
-// implicit return
-const Person = () => <h2>john doe</h2>;
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/41UGPOZqH2L._AC_SF480,480_.jpg"
+    alt="Hunt It Down, Kill It & Drag It Home"
+  />
+);
 
-// Explicit return
-const Message = () => {
-  return <p>this is my message</p>;
+const Title = () => <h2> Hunt It Down, Kill It & Drag It Home</h2>;
+
+const Author = () => {
+  return <h4>David Thomas Roberts</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // console.log(root);
 
-root.render(<Greeting />);
+root.render(<BookList />);
 
-/* same as root.render(<Greeting></Greeting>); */
+/* same as root.render(<BookList></BookList>); */
