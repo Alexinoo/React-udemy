7472cbd27265868af75a7744d2335220bb5ca87e
Alex Mwangi <alex@stl-horizon.com>
2023-01-31 16:06:18 +0300

    React Fundamentals - JSX Javascript

--- 00-react-tutorial-app/src/index.js
+++ 00-react-tutorial-app/src/index.js
@@ -12,33 +12,20 @@ const BookList = () => {
     </section>
   );
 };
+const author = 'Jordan Moore';
+
 const Book = () => {
+  const title = 'Interesting Facts about curious minds';
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="./images/Curious_minds.jpg"
+        alt="Interesting Facts about curious minds"
+      />
+      <h2>{title} </h2>
+      <h4>{author.toUpperCase()}</h4>
     </article>
   );
-};
-
-const Image = () => (
-  <img
-    // src="https://m.media-amazon.com/images/I/41UGPOZqH2L._AC_SF480,480_.jpg"
-    src="./images/Curious_minds.jpg"
-    alt="Interesting Facts about curious minds"
-  />
-);
-
-const Title = () => <h2> Interesting Facts about curious minds</h2>;
-
-const Author = () => {
-  const inlineHeadingStyles = {
-    color: '#617d98',
-    fontSize: '0.75rem',
-    marginTop: '0.5rem',
-  };
-  return <h4 style={inlineHeadingStyles}>Jordan Moore</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
