a32db82a21206d288739b2b868aed8f0edee3bb3
Alex Mwangi <alex@stl-horizon.com>
2023-02-01 14:32:34 +0300

    React Fundamentals - Somewhat Dynamic Setup

--- 00-react-tutorial-app/src/index.js
+++ 00-react-tutorial-app/src/index.js
@@ -2,15 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
 
-const author = 'Jordan Moore';
-const title = 'Interesting Facts about curious minds';
-const img = './images/Curious_minds.jpg';
+const firstBook = {
+  author: 'Colleen Hover',
+  title: 'IT ENDS WITH US',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL600_SR600,400_.jpg',
+};
+
+const secondBook = {
+  author: 'Jay Shetty',
+  title: '8 RULES OF LOVE',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/71FI7MAeUcL._AC_UL600_SR600,400_.jpg',
+};
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book author={author} title={title} img={img} />
-      <Book author={author} title={title} img={img} />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
 };
