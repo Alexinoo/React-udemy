de6534c272c12beade7260c2fb1b716e3b2ace00
Alex Mwangi <alex@stl-horizon.com>
2023-02-01 12:53:13 +0300

    React Fundamentals - Props Basic Setup

--- 00-react-tutorial-app/src/index.js
+++ 00-react-tutorial-app/src/index.js
@@ -2,28 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
 
+const author = 'Jordan Moore';
+const title = 'Interesting Facts about curious minds';
+const img = './images/Curious_minds.jpg';
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book author={author} title={title} img={img} />
+      <Book author={author} title={title} img={img} />
     </section>
   );
 };
-const author = 'Jordan Moore';
 
-const Book = () => {
-  const title = 'Interesting Facts about curious minds';
+const Book = (props) => {
   return (
     <article className="book">
-      <img
-        src="./images/Curious_minds.jpg"
-        alt="Interesting Facts about curious minds"
-      />
-      <h2>{title} </h2>
-      <h4>{author.toUpperCase()}</h4>
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title} </h2>
+      <h4>{props.author.toUpperCase()}</h4>
     </article>
   );
 };
