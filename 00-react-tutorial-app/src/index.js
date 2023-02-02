139699b3d971a2b90c2a9059f8617f55fb395ecb
Alex Mwangi <alex@stl-horizon.com>
2023-02-02 17:09:57 +0300

    React Fundamentals - Props Drilling

--- 00-react-tutorial-app/src/index.js
+++ 00-react-tutorial-app/src/index.js
@@ -18,25 +18,29 @@ const books = [
 ];
 
 const BookList = () => {
+  const someValue = 'shakeAndBake';
+
+  const displayValue = () => {
+    console.log(someValue);
+  };
+
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book {...book} key={book.id} />;
+        return <Book {...book} key={book.id} displayValue={displayValue} />;
       })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { author, title, img } = props;
-  const displayTitle = () => {
-    console.log(title);
-  };
+  const { author, title, img, displayValue } = props;
+
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title} </h2>
-      <button onClick={displayTitle}>display title</button>
+      <button onClick={displayValue}>click me</button>
       <h4>{author.toUpperCase()}</h4>
     </article>
   );
