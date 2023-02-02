0cec9d160a7819190105bb284fc4cda0bfb63e42
Alex Mwangi <alex@stl-horizon.com>
2023-02-02 21:58:37 +0300

    Local Images (src folder)

--- 00-react-tutorial-app/src/books.js
+++ 00-react-tutorial-app/src/books.js
@@ -1,14 +1,24 @@
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpg';
+import img3 from './images/book-3.jpg';
+
 export const books = [
   {
-    author: 'Colleen Hover',
-    title: 'IT ENDS WITH US',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL600_SR600,400_.jpg',
+    author: 'Robert Greene',
+    title: 'The 48 Laws Of Power',
+    img: img2,
     id: 1,
   },
   {
-    author: 'Jay Shetty',
-    title: '8 RULES OF LOVE',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/71FI7MAeUcL._AC_UL600_SR600,400_.jpg',
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: img1,
     id: 2,
+  },
+  {
+    author: 'Robert T. Kiyosaki',
+    title: 'Rich Dad Poor Dad',
+    img: img3,
+    id: 3,
   },
 ];
