0618ca03fb89a31b5bb61448fc0282672c66bc5a
Alex Mwangi <alex@stl-horizon.com>
2023-02-02 11:04:06 +0300

    React Fundamentals - Simple list

--- 00-react-tutorial-app/src/index.js
+++ 00-react-tutorial-app/src/index.js
@@ -2,41 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
 
-const firstBook = {
-  author: 'Colleen Hover',
-  title: 'IT ENDS WITH US',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL600_SR600,400_.jpg',
-};
+const books = [
+  {
+    author: 'Colleen Hover',
+    title: 'IT ENDS WITH US',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL600_SR600,400_.jpg',
+  },
+  {
+    author: 'Jay Shetty',
+    title: '8 RULES OF LOVE',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71FI7MAeUcL._AC_UL600_SR600,400_.jpg',
+  },
+];
 
-const secondBook = {
-  author: 'Jay Shetty',
-  title: '8 RULES OF LOVE',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/71FI7MAeUcL._AC_UL600_SR600,400_.jpg',
-};
+const names = ['john', 'peter', 'susan'];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>;
+});
 
 const BookList = () => {
-  return (
-    <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-          eligendi voluptate ipsa voluptatibus reprehenderit obcaecati animi in
-          eveniet nisi cumque!
-        </p>
-        <button>click me</button>
-      </Book>
-
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
-    </section>
-  );
+  return <section className="booklist">{newNames}</section>;
 };
 
 const Book = (props) => {
