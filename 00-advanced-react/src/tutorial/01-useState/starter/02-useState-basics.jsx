c964649b07899ec94321d270b8ed91e745f5d591
Alex Mwangi <alex@stl-horizon.com>
2023-02-09 14:26:23 +0300

    useState Fundamentals

--- 00-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ 00-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,35 @@
+import { useState } from 'react';
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  //////////////////////
+  // What is useState()..?
+  // console.log(useState()); [undefined, ƒ];
+
+  // Accessing individual elements since we are getting array
+  // const value = useState('hello')[0];
+  // const handler = useState('hello')[1];
+  // console.log(value);
+  // console.log(handler);
+
+  // Why Array destructuring is so important - one liner
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    // invoke setCount()
+    // Technically , we can pass anything
+    // If we pass a string of Bob , we will get a string of Bob
+    // Something to be aware of but in our case we will increase the count
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h4>You clicked {count} times</h4>
+      <button type="button" className="btn" onClick={handleClick}>
+        click me
+      </button>
+    </div>
+  );
 };
 
 export default UseStateBasics;
