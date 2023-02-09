a3948123e829ceb5c3ac93fc0a6612c1904b3f94
Alex Mwangi <alex@stl-horizon.com>
2023-02-09 17:06:25 +0300

    useState Object - Setup

--- 00-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ 00-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,30 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name, setName] = useState('Alex');
+  const [age, setAge] = useState(32);
+  const [hobby, setHobby] = useState('Reading Books');
+
+  const displayPerson = () => {
+    setName('John');
+    setAge(30);
+    setHobby('Hiking');
+  };
+
+  return (
+    <React.Fragment>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>Enjoys : {hobby}</h3>
+      <button
+        className="btn"
+        style={{ marginTop: '2rem' }}
+        onClick={displayPerson}
+      >
+        show john
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateObject;
