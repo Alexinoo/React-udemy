7b52359643978ccd2f1b5000d1607933f7af0b90
Alex Mwangi <alex@stl-horizon.com>
2023-02-09 17:26:52 +0300

    useState Object - Complete object setup

--- 00-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ 00-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,21 +1,34 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  const [name, setName] = useState('Alex');
-  const [age, setAge] = useState(32);
-  const [hobby, setHobby] = useState('Reading Books');
+  const [person, setPerson] = useState({
+    name: 'Alex',
+    age: 32,
+    hobby: 'Reading Books',
+  });
 
   const displayPerson = () => {
-    setName('John');
-    setAge(30);
-    setHobby('Hiking');
+    // Be aware of the gotchas
+    // Remember we can pass whatever value in the setPerson()
+    // Even though , this is an object, we can pass a string and our application is going to break because there are no more person.name,person.age or person.hobby anymore
+    // setPerson({ name: 'John', age: 24, hobby: 'Hiking' });
+    // setPerson('shakeandbake');
+
+    // We can also change only one and still our app will break
+    // setPerson({ name: 'susan' });
+
+    // SOLUTION :
+    //  Copy the properties from the state {...person,}
+    // decide which ones we want to ovewrite {...person,name: 'John'}
+    // In our case , all
+    setPerson({ ...person, name: 'John', age: '24', hobby: 'Hiking' });
   };
 
   return (
     <React.Fragment>
-      <h3>{name}</h3>
-      <h3>{age}</h3>
-      <h3>Enjoys : {hobby}</h3>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>Enjoys : {person.hobby}</h3>
       <button
         className="btn"
         style={{ marginTop: '2rem' }}
