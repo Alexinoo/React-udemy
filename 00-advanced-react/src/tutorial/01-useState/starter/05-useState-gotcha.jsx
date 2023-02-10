0d695d525faebe7fa3fa78852b584e7bca824467
Alex Mwangi <alex@stl-horizon.com>
2023-02-10 11:49:00 +0300

    useState setFunction Gotcha

--- 00-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 00-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,25 @@
+import { useState } from 'react';
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [value, setValue] = useState(0);
+
+  const handleClick = () => {
+    setValue(value + 1);
+    //  be careful it's the old value
+    console.log(value);
+    //  so if you have any functionality
+    // that relies on the latest value
+    // it will be wrong !!!
+  };
+
+  return (
+    <div>
+      <h3>{value}</h3>
+      <button type="button" className="btn" onClick={handleClick}>
+        Increase
+      </button>
+    </div>
+  );
 };
 
 export default UseStateGotcha;
