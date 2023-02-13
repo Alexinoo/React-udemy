c9ef41ac958f69fce29f5724c7254c74401e0c1b
Alex Mwangi <alex@stl-horizon.com>
2023-02-13 16:04:45 +0300

    Multiple Returns - Fetch Data Setup

--- 00-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 00-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,6 +2,24 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getData = async (data_url) => {
+      try {
+        const response = await fetch(data_url);
+        if (!response.ok) {
+          throw new Error('resource not found');
+        }
+        const data = await response.json();
+        setUser(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getData(url);
+  }, []);
+
   return <h2>Fetch Data </h2>;
 };
 export default MultipleReturnsFetchData;
