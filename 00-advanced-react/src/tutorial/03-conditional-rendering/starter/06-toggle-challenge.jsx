ddf3413d5fe4c9d5356ba8203671e87d8ed93023
Alex Mwangi <alex@stl-horizon.com>
2023-02-14 09:50:50 +0300

    Toggle Challenge

--- 00-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ 00-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,33 @@
+import { useState } from 'react';
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [showAlert, setShowAlert] = useState(true);
+
+  // Reference
+  // const toggleAlert = () => {
+  //   // if (showAlert) {
+  //   //   setShowAlert(false);
+  //   //   return;
+  //   // }
+  //   // setShowAlert(true);
+
+  //   if (showAlert) {
+  //     setShowAlert(false);
+  //   } else {
+  //     setShowAlert(true);
+  //   }
+  // };
+  return (
+    <div>
+      <button className="btn" onClick={() => setShowAlert(!showAlert)}>
+        Toggle
+      </button>
+      {showAlert && <Alert />}
+    </div>
+  );
+};
+
+const Alert = () => {
+  return <div className="alert alert-danger">hello there</div>;
 };
 
 export default ToggleChallenge;
