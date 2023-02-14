05ba8ce25fb33cfff64c6d9a589d3af9e0ffa6e6
Alex Mwangi <alex@stl-horizon.com>
2023-02-14 10:50:49 +0300

    useEffect Cleanup Function - Setup

--- 00-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 00-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from 'react';
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [isToggle, setisToggle] = useState(false);
+  return (
+    <div>
+      <button
+        type="button"
+        className="btn"
+        onClick={() => setisToggle(!isToggle)}
+      >
+        toggle component
+      </button>
+      {isToggle && <RandomComponent />}
+    </div>
+  );
+};
+
+const RandomComponent = () => {
+  useEffect(() => {
+    console.log('hmm, this is interesting');
+  }, []);
+  return <h3 style={{ marginTop: '2rem' }}>Hello from Some Component</h3>;
 };
 
 export default CleanupFunction;
