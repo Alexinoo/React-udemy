6face4f31aa379ffdc490d7212a8af3b367f738d
Alex Mwangi <alex@stl-horizon.com>
2023-02-14 10:16:26 +0300

    Login/Logout User Challenge

--- 00-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ 00-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,47 @@
+import { useState } from 'react';
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+
+  const login = () => {
+    // normally connect to db or api
+    setUser({ name: 'Alex' });
+  };
+
+  const logout = () => {
+    // destroy/clear session
+    setUser(null);
+  };
+  return (
+    <div>
+      {user ? (
+        <Logout name={user.name} logout={logout} />
+      ) : (
+        <Login login={login} />
+      )}
+    </div>
+  );
+};
+
+const Login = ({ login }) => {
+  return (
+    <div>
+      <h4>Please login</h4>
+      <button className="btn" onClick={login}>
+        login
+      </button>
+    </div>
+  );
+};
+const Logout = ({ name, logout }) => {
+  return (
+    <div>
+      <h4>Hello there user : {name}</h4>
+      <button className="btn" onClick={logout}>
+        logout
+      </button>
+    </div>
+  );
 };
 
 export default UserChallenge;
