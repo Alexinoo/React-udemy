f6d1e728902e2f721aa76d7ba117252dad76bf09
Alex Mwangi <alex@stl-horizon.com>
2023-02-15 11:33:56 +0300

    Forms - Add/Remove Users challenge

--- 00-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ 00-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,20 +1,68 @@
+import { useState } from 'react';
+import { data } from '../../../data';
 const UserChallenge = () => {
+  const [name, setName] = useState('');
+  const [users, setUsers] = useState(data);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // check if the user tries to submit empty name ; return
+    if (!name) return;
+
+    // Add new user to the list
+    const newUser = {
+      id: Date.now(),
+      name,
+    };
+    console.log(newUser);
+    //  Push to the list array -- Won't work
+    // const updatedUsers = users.push(newUser);
+
+    // Use spread operator instead
+    const updatedUsers = [...users, newUser];
+    setUsers(updatedUsers);
+
+    // Empty the input
+    setName('');
+  };
+
+  // remove user
+  const removeUser = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
   return (
     <div>
-      <form className='form'>
+      <form className="form" onSubmit={handleSubmit}>
         <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
-      {/* render users below */}
+      <h2>users</h2>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <h4>{user.name}</h4>
+            <button className="btn" onClick={() => removeUser(user.id)}>
+              remove
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 };
